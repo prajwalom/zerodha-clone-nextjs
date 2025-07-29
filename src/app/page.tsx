3e346920193dc059924e925aecd917daedca4f6b
prajwalom <prajwalpadole99@gmail.com>
2025-07-29 11:36:42 +0530

    added resizable nabar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
-import { cn } from "@/lib/utils";
-import { Marquee } from "@/components/magicui/marquee";
+
 import { MarqueeDemo } from "@/components/magicui/marquee";
+import { NavbarDemo } from "@/components/ui/resizable-navbar";
+
+import React, { useRef, useState } from "react";
 
 export default function HomePage() {
   return (
     <div>
-      {/* Other content of your HomePage can go here */}
-      <h1>Welcome to My Homepage</h1>
-      <p>This is some content above the marquee.</p>
-
-      {/* Add the MarqueeDemo component */}
+      <NavbarDemo/>
       <MarqueeDemo />
-
-      <p>This is some content below the marquee.</p>
     </div>
   );
 }
